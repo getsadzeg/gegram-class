5aa1f9ed793ea0070833995c51cc5f39955a3a25
getsadzeg <getsadzeguram@gmail.com>
2015-09-25 23:34:36 +0400

    first grammar fixing

---
+++ main.java
@@ -0,0 +1,19 @@
+public class main {
+	public static void main(String []args) {
+		String input[] = {"დღეს", "წიგნი", "არ", "წამიკითხია"};
+		boolean isMistake = false;
+		String lastWord;
+		char lastChar;
+		char beforeLast;
+		for(int i=0; i<input.length; i++) {
+			lastWord = input[input.length-1];
+			System.out.println(lastWord);
+			lastChar = lastWord.charAt(lastWord.length()-1);
+			beforeLast = lastWord.charAt(lastWord.length()-2);
+			if(lastChar == 'ა' && beforeLast == 'ი') isMistake = true;
+			System.out.println(isMistake);
+			break;
+			//if(lastWord.charAt(lastWord.length()-1) == 'ა' && lastWord.charAt(lastWord.length()-2) == 'ი') isMistake == true;
+		}
+	}
+}
