d3a4bda1b5f415aac278c3dc39c6d32751c8b616
getsadzeg <getsadzeguram@gmail.com>
2015-10-03 21:04:44 +0400

    now gegram fixes mistakes in  Exceptions' source

--- src/Exceptions.java
+++ src/Exceptions.java
@@ -1,18 +1,21 @@
 import java.util.Arrays;
 public class Exceptions {
 	public static void main(String[] args) {
-		String input[] = {"მე", "მჯერავს", "სიყვარული"};
-		String exceptions[] = {"ბრძოლობდა", "მჯერავს", "შეურაწყოფა", "შეურაცყოფა"};
+		String input[] = {"მე", "მჯერავს", "საქართველოს", "გაბრწყინების"};
+		String exceptions[] = {"ბრძოლობდა", "მჯერავს", "შეურაწყოფა", "შეურაცყოფა", "ნაყურები"};
+		String fixes[] = {"იბრძოდა", "მჯერა", "შეურაცხყოფა", "შეურაცხყოფა", "ნანახი"};
 		boolean isMistake = false;
-		for(int i=0; i<input.length; i++) {
-			for(String c : exceptions) {
-				if(input[i] == c) {
+		int count = 0;
+		int index = 0;
+		for(int i=0; i<exceptions.length; i++) {
+			for(String c : input) {
+				if(exceptions[i] == c) {
 					isMistake = true;
-					System.out.println(c + "Detected");
+					System.out.println(c + " Detected");
+					System.out.println("Mistake fix: " + fixes[i]);
 				}
 			}
 		}
 		if(isMistake == false) System.out.println("No detections");
 	}
-
 }
